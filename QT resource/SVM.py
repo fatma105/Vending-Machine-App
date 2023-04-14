26794dc34bf7f1354bfa4b6c948ec2148d01283d
fatma105 <63948537+fatma105@users.noreply.github.com>
2023-04-15 01:24:37 +0200

    update

--- QT resource/SVM.py
+++ QT resource/SVM.py
@@ -25,8 +25,7 @@ class mainapp(QMainWindow,FORM_CLASS):
 
 #logo this is name for logo PushButton ,i changed it in qt designer
     def Handle_Button(self):
-        Open_page=NextPage()
-        page=Open_page.go_to_page2()
+        page=NextPage().go_to_page2()
         self.logo.clicked.connect(page)
 
 
@@ -35,15 +34,52 @@ class NextPage(QtWidgets.QMainWindow):
     def __init__(self):
         super(NextPage, self).__init__()
         self.go_to_page2()
-        self.show()
+        self.Handle_lcd()
+        #self.show()
 
     def go_to_page2(self):
-       uic.loadUi('Home.ui', self)
+        loadUiType(path.join(path.dirname(__file__),"Home.ui"))
+
+    def Handle_lcd(self):
+        # Create LCD Number and set its properties
+        self.lcd = QLCDNumber(self)
+        self.lcd.display(0)
+        self.lcd.setSegmentStyle(QLCDNumber.Flat)
+        self.lcd.setFixedSize(50, 50)
+
+        # Create Plus Button and set its properties
+        self.plus_button = QPushButton("+", self)
+        self.plus_button.setFixedSize(30, 30)
+        self.plus_button.clicked.connect(self.increaseNumber)
+        # Create Subtract Button and set its properties
+        self.subtract_button = QPushButton("-", self)
+        self.subtract_button.setFixedSize(30, 30)
+        self.subtract_button.clicked.connect(self.decreaseNumber)
+        # Create horizontal Box Layout and add widgets to it
+        hbox = QVBoxLayout()
+        hbox.addWidget(self.lcd)
+        hbox.addWidget(self.plus_button)
+        hbox.addWidget(self.subtract_button)
+        self.setLayout(hbox)
+
+    def increaseNumber(self):
+        num = int(self.lcd.value())
+        if num < 100:
+            num += 1
+            self.lcd.display(num)
+
+    def decreaseNumber(self):
+        num = int(self.lcd.value())
+        if num > 0:
+            num -= 1
+            self.lcd.display(num)
 
 def main():
     app = QApplication(sys.argv)
     window = mainapp()
     window.show()
+    # Nextwindow=NextPage()
+    # Nextwindow.show()
     app.exec_()
 if __name__=='__main__':
     main()
