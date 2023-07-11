255346bddd7667c541cc1011b93a7e4b23124431
shazlee00 <72980547+shazlee00@users.noreply.github.com>
2023-07-11 11:19:25 +0300

    hardware

--- QT resource/Hardware.py
+++ QT resource/Hardware.py
@@ -1,2 +1,26 @@
-def dispense_items(order_list):
-    pass
+import serial
+
+def dispense_items(data):
+    # Configure the serial port
+    ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace '/dev/ttyUSB0' with your actual serial port
+    ser.timeout = 1  # Set the timeout value (in seconds)
+
+    try:
+        # Open the serial port
+        if not ser.is_open:
+            ser.open()
+
+        # Send the data to the device
+        ser.write(data.encode())
+
+        # Wait for a response (if expecting one)
+        response = ser.readline().decode().strip()
+        print("Response:", response)
+
+    except serial.SerialException as e:
+        print("Serial port error:", str(e))
+
+    finally:
+        # Close the serial port
+        if ser.is_open:
+            ser.close()
